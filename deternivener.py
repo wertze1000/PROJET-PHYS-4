c16cc9c2f04a77c5b58c8bef7491d9da4ba920da
Helojand <127391037+Helojand@users.noreply.github.com>
2023-05-16 13:38:11 +0200

    Update deternivener.py

--- deternivener.py
+++ deternivener.py
@@ -11,14 +11,15 @@ def piecewise_approximation(f, x_range, num_pieces):
     y = f(x)
     approx_y = np.zeros_like(y)
     piece_width = (x_max - x_min) / num_pieces
-
+    
+    start = 0
     for i in range(num_pieces):
         x_piece = np.linspace(x_min + i*piece_width, x_min + (i+1)*piece_width, 1000//num_pieces)
         y_piece = f(x_piece)
         approx_y_piece = np.full_like(y_piece, np.mean(y_piece))
         indices = np.where((x >= x_min + i*piece_width) & (x <= x_min + (i+1)*piece_width))
         approx_y[indices] = approx_y_piece
-    return x, y, approx_y
+    return x, y, approx_y, start
 def k(E, approx_y):
     m=cst.electron_mass
     E0 = np.array([])
@@ -43,23 +44,26 @@ def Mx(k, b):
 def M(n, E, Eo, sigma):
     f = lambda x: lennardJones(x, Eo, sigma)
     x_range = (0.01, 1.3)
-    x, y, approx_y = piecewise_approximation(f, x_range, n)
+    x, y, approx_y, a = piecewise_approximation(f, x_range, n)
     K, c = k(E, approx_y)
     M = np.zeros((2,2))
     for i in range(n):
         Mi = Mx(K[n-i], c[n-i])
         if (i == 0):
+            k1 = K[i]
             M = Mi
         else:
             M = np.linalg.inv(Mi)*M
-    return M
+    return M, k1, a
 
-Etest = np.linspace(0, 1e-15, 100000)
+
 def nbener(n, Eo, sigma):
+    Etest = np.linspace(-Eo, 0, 1000)
     s = 0
     Mt = np.array([])
     for e in Etest:
-        Mt = np.append(Mt, M(n, e, Eo, sigma)[0][0])
+        m, k1, a = M((n, e, Eo, sigma))
+        Mt = np.append(Mt, m[0][0]+m[0][1]*(1/math.tan(k1*a)))
     e = Mt[argrelextrema(Mt, np.less)[0]]
     s = len(e)
     return s, e
