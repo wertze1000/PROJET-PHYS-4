b319f4bb36ac0bb485ebb7e6aa2e7b6b9517a1a5
Helojand <127391037+Helojand@users.noreply.github.com>
2023-05-18 15:46:46 +0200

    Update approximation.py

--- approximation.py
+++ approximation.py
@@ -6,22 +6,21 @@ import matplotlib.pyplot as plt
 Eo = 0.45
 sigma = 0.45
 
-#FUNCTIONS
-def lennardJones(r):
+def lennardJones(r, Eo, sigma):
         lj = (4*Eo*((sigma/r)**12 - (sigma/r)**6))
         return lj 
 
-def approx(arbitraryFunction, xmin, xmax, nWells):
-    
+def approx(arbitraryFunction, xmin, xmax, nWells, Eo, sigma):
+    nWells+=1
     xSpace = np.linspace(xmin, xmax, 1000, retstep=True)    #Set of points whithin which the function should be welled, with a set precision
     step = xSpace[1]                                        #linspace gives the step thanks to the retstep argument
-    functionMinimum = min(arbitraryFunction(xSpace[0]))     #The minimum of the function within the desired space
+    functionMinimum = min(arbitraryFunction(xSpace[0], Eo, sigma))     #The minimum of the function within the desired space
     
     approx = []                                             #Empty list that will be filled with the calculations
 
     #Offsets the entire arbitrary function to avoid negative integrals (so the function is always positive)
     def f(x):                                               
-         return arbitraryFunction(x) + np.abs(functionMinimum)
+         return arbitraryFunction(x, Eo, sigma) + np.abs(functionMinimum)
     
     #Dividing the total area calculated with an integral by the number of wells ( that should have an equal area )
     area = np.abs(integrate.quad(f, xmin, xmax))
@@ -78,14 +77,21 @@ def approx(arbitraryFunction, xmin, xmax, nWells):
 #EXECUTION
 x_min, x_max = (0.4, 1.25)
 x = np.linspace(x_min, x_max, 1000)
-y = lennardJones(x)
+y = lennardJones(x, Eo, sigma)
 
 #PLOT
 plt.plot(x, y, color = "green",label = "Potentiel de Lennard-Jones")
-plt.plot(x, approx(lennardJones, x_min, x_max, 2), color = "blue", linestyle = "dotted", label = "ordre 2")
-plt.plot(x, approx(lennardJones, x_min, x_max, 10), color = "red", linestyle = "dashed", label = "ordre 10")
+plt.plot(x, approx(lennardJones, x_min, x_max, 1, Eo, sigma), color = "blue", linestyle = "dotted", label = "ordre 1")
+plt.plot(x, approx(lennardJones, x_min, x_max, 10, Eo, sigma), color = "red", linestyle = "dashed", label = "ordre 10")
 
 plt.axhline(y = 0, color = 'black')
+
+plt.legend()
+plt.ylabel("V(r) [eV]")
+plt.xlabel("r [nm]")
+plt.ylim(top = 1.5, bottom = -1)
+plt.xlim(left = 0.1, right = 1.5)
+plt.show()
 
 plt.legend()
 plt.ylabel("V(r) [eV]")
